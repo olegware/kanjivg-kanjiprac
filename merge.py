fb6b9d08a2ffbe2790ca7f862595d51c43754b37
olegware <129217985+olegware@users.noreply.github.com>
2023-04-01 01:58:51 +0000

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,19 @@
+import os
+import xml.etree.ElementTree as ET
+
+kanji_folder = "C:/Users/Viktor/Desktop/kanji"
+output_file = "kanjivg_merged.xml"
+
+kanji_files = [f for f in os.listdir(kanji_folder) if f.endswith(".svg")]
+
+root = ET.Element("kanjivg")
+root.set("xmlns", "http://kanjivg.tagaini.net")
+
+for filename in kanji_files:
+    file_path = os.path.join(kanji_folder, filename)
+    tree = ET.parse(file_path)
+    kanji_element = tree.getroot()
+    root.append(kanji_element)
+
+merged_tree = ET.ElementTree(root)
+merged_tree.write(output_file, encoding="utf-8", xml_declaration=True)
